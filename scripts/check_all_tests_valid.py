feb958c1b1ccc64e8eb3fc8de99f7445dcd59718
mmmity <mmmityk@yandex.ru>
2026-04-19 22:24:30 +0300

    return all checkers

--- scripts/check_all_tests_valid.py
+++ scripts/check_all_tests_valid.py
@@ -2,7 +2,7 @@ import os
 import sys
 import subprocess
 
-PREFIXES = ['facility']
+PREFIXES = ['setcover', 'knapsack', 'coloring', 'tsp', 'facility']
 
 def run_all_tests(prefix):
   print(f'Doing prefix {prefix}')
